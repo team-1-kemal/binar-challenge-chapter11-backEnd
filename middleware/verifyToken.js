1e7387a388b7c37acc0a31e3fedcf99504b7fd70
fureya48 <fathur.rohman2535@gmail.com>
2022-11-01 19:48:22 +0700

    untrack .env file from git

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,16 +1,15 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-export const verifyToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token == null)
-    return res.status(401).send('token is required for authnetication');
+module.exports = verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.status(401).send("token is required for authnetication");
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.userId = decoded.data;
     req.token = token;
     return next();
   } catch (err) {
-    return res.status(403).send('token is invalid');
+    return res.status(403).send("token is invalid");
   }
 };
