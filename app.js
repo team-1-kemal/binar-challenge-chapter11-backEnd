11152cc29ded937d644c548d91c04439b690d1d6
ihsan <karamiihsan@gamil.com>
2022-11-08 20:39:09 +0700

    fix conflict

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
 const express = require('express');
-
-=======
-const express = require("express");
->>>>>>> bf2620a3067d24ba60780b3a453778155c7c99d9
 const app = express();
-const db = require("./models");
-const cors = require("cors");
-const router = require("./routes");
+const db = require('./models');
+const cors = require('cors');
+const router = require('./routes');
 
 const corsOptions = {
-  origin: "http://localhost:5000",
+  origin: 'http://localhost:5000',
 };
 
 app.use(express.json());
@@ -21,7 +16,7 @@ app.use(router);
 db.sequelize
   .authenticate()
   .then(() => {
-    console.log("database connected");
-    app.listen(5000, () => console.log("serven on at http://localhost:5000"));
+    console.log('database connected');
+    app.listen(5000, () => console.log('serven on at http://localhost:5000'));
   })
   .catch((error) => console.log(error));
