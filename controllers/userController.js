98ce155a9425b957dfe08948531ef7d7a3adcf2d
ihsan <karamiihsan@gamil.com>
2022-11-09 23:02:24 +0700

    add user update and delete controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,9 +5,10 @@ module.exports = {
   updateUser: async (req, res) => {
     try {
       const user = await Users.find(
+        req.params.userId,
         req.body.full_name,
         req.body.email,
-        req.params.dob,
+        req.body.dob,
         req.body.city
       );
       if (!user) {
@@ -23,7 +24,25 @@ module.exports = {
     } catch (error) {
       return res.status(500).json({
         success: false,
-        message: 'Server error. Please try again.',
+        message: 'Server error',
+        error: error.message,
+      });
+    }
+  },
+  deleteUser: async (req, res) => {
+    try {
+      const user = await Users.findByIdAndDelete(req.params.userId);
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.status(201).json({
+        success: true,
+        message: 'delete successfull',
+      });
+    } catch (error) {
+      return res.status(501).json({
+        success: false,
+        message: 'Server error',
         error: error.message,
       });
     }
